db7d08634ee2a3fba4e82962b927c7e709b12992
szczm <szczm@users.noreply.github.com>
2020-07-08 21:41:55 +0200

    Added return functionality, updated UX

--- teleporty.js
+++ teleporty.js
@@ -1,4 +1,5 @@
 let audibleTab = null;
+let lastTabId = null;
 
 async function findFirstAudibleTab() {
   let tabs = await browser.tabs.query({
@@ -7,26 +8,97 @@ async function findFirstAudibleTab() {
   });
 
   if (tabs.length > 0) {
-    return tabs[0].id;
+    return tabs[0];
   }
+}
+
+async function isAudibleTabActive() {
+  if (audibleTab == null) {
+    audibleTab = await findFirstAudibleTab();
+
+    if (audibleTab == null) {
+      return;
+    }
+  }
+
+  let tabs = await browser.tabs.query({ active: true });
+
+  return tabs.some(tab => tab.id == audibleTab.id);
 }
 
 async function teleport(tab) {
   if (audibleTab == null) {
     audibleTab = await findFirstAudibleTab();
+
+    if (audibleTab == null) {
+      return;
+    }
   }
 
+  let targetTabId = await isAudibleTabActive() ? lastTabId : audibleTab.id;
+    
   // teleport to the audible tab
-  browser.tabs.update(audibleTab, {
+  browser.tabs.update(targetTabId, {
     active: true
   }).catch();
 }
 
-function processTabUpdate(tabId, currentInfo) {
-  if (currentInfo.audible === true) {
-    audibleTab = tabId;
+async function updateIcon() {
+  if (await isAudibleTabActive() == true) {
+    setAudibleActiveIcon();
+  } else {
+    setAudibleInactiveIcon();
   }
 }
 
-browser.tabs.onUpdated.addListener(processTabUpdate);
+function setAudibleInactiveIcon() {
+  browser.browserAction.setIcon({
+    path: {
+      16: "/icons/teleporty-16.png",
+      32: "/icons/teleporty-32.png",
+    }
+  });
+}
+
+function setAudibleActiveIcon() {
+  browser.browserAction.setIcon({
+    path: {
+      16: "/icons/teleporty-active-16.png",
+      32: "/icons/teleporty-active-32.png",
+    }
+  });
+}
+
+function assignAudibleTab(tabId, currentInfo, tab) {
+  if (currentInfo.audible === true) {
+    audibleTab = tab;
+  }
+}
+
+function assignLastTab(activeInfo) {
+  if (audibleTab && audibleTab.windowId === activeInfo.windowId) {
+    lastTabId = activeInfo.previousTabId;
+  }
+}
+
+function checkRemovedTab(tabId) {
+  if (audibleTab && audibleTab.id == tabId) {
+    audibleTab = null;
+  }
+
+  if (lastTabId == tabId) {
+    lastTabId = null;
+  }
+}
+browser.tabs.onUpdated.addListener(assignAudibleTab);
+browser.tabs.onActivated.addListener(assignLastTab);
+browser.tabs.onRemoved.addListener(checkRemovedTab)
+
+browser.tabs.onUpdated.addListener(updateIcon);
+browser.tabs.onActivated.addListener(updateIcon);
+browser.tabs.onRemoved.addListener(updateIcon);
+
+updateIcon();
+
 browser.browserAction.onClicked.addListener(teleport);
+// browser.browserAction.onClicked.addListener(teleport);
